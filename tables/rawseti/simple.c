57d4cac98446b5f1a14e5b62917f94007fd8584a
Kentaro Hayashi <hayashi@clear-code.com>
2020-12-22 19:31:37 +0900

    Add rawseti sample

---
+++ tables/rawseti/simple.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main(void)
+{
+  lua_State *L = luaL_newstate();
+  int status = 0;
+
+  luaL_openlibs(L);
+
+  lua_newtable(L);
+
+  lua_newtable(L);
+
+  lua_pushstring(L, "Meow");
+  lua_setfield(L, -2, "cat");
+
+  lua_pushstring(L, "Bow");
+  lua_setfield(L, -2, "dog");
+
+  lua_rawseti(L, -2, 1);
+
+  lua_setglobal(L, "table");
+
+  status = luaL_dostring(L, "print(table[1].cat)");
+  if (status) {
+    printf("Error: %s", lua_tostring(L, -1));
+  }
+
+  status = luaL_dostring(L, "print(table[1].dog)");
+  if (status) {
+    printf("Error: %s", lua_tostring(L, -1));
+  }
+  return 0;
+}
+
+  
