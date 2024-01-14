dc8e3afaf945c09a37cb09baea5b8f6eb2831d2c
Kentaro Hayashi <kenhys@gmail.com>
2024-01-14 18:12:36 +0900

    Add comment

--- tables/simple/setfield.c
+++ tables/simple/setfield.c
@@ -12,12 +12,29 @@ int main(void)
 
   lua_newtable(L);
 
+  /*
+    {
+      "cat" => "Meow",
+    }
+  */
   lua_pushstring(L, "Meow");
   lua_setfield(L, -2, "cat");
 
+  /*
+    {
+      "cat" => "Meow",
+      "dog" => "Bow",
+    }
+  */
   lua_pushstring(L, "Bow");
   lua_setfield(L, -2, "dog");
 
+  /*
+    table => {
+      "cat" => "Meow",
+      "dog" => "Bow",
+    }
+  */
   lua_setglobal(L, "table");
 
   status = luaL_dostring(L, "print(table.cat)");
